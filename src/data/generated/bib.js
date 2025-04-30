c057d0b288f3da8887a192fd76ae881d3ca93220
Simon <simon@SimondeMacBook-Pro.local>
2025-04-30 18:35:44 +0100

    Regenerated bib.js after recloning

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,102 @@
 ﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+    "Ali2023": {
+        "author": "Ali, A. and Zafar, M.",
+        "doi": "10.1108/ICS-09-2023-0156",
+        "journal": "Information & Computer Security",
+        "keywords": "type:journal, usability",
+        "series": "ICS",
+        "title": "Unraveling the Dynamics of Password Manager Adoption: A Deeper Understanding",
         "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
+        "year": "2023"
+    },
+    "Andersson2008": {
+        "author": "Andersson, J. and Pettersson, O.",
+        "doi": "10.1145/1572532.1572579",
+        "journal": "NordiCHI",
+        "keywords": "type:conference, password-generation",
+        "series": "NordiCHI",
+        "title": "Graphical Passwords as Browser Extension: Implementation and Usability Study",
+        "type": "article",
+        "year": "2008"
+    },
+    "Chen2022": {
+        "author": "Chen, L. and Kim, H.",
+        "doi": "10.1145/3477314.3507098",
+        "journal": "Proceedings of CODASPY",
+        "keywords": "type:conference, plugin-security",
+        "series": "CODASPY",
+        "title": "Hardening the Security Analysis of Browser Extensions",
+        "type": "article",
+        "year": "2022"
+    },
+    "Furnell2018": {
+        "author": "Furnell, S. and Clarke, N.",
+        "doi": "10.1016/j.cose.2018.08.004",
+        "journal": "Computers & Security",
+        "keywords": "type:journal, usability",
+        "series": "CoSE",
+        "title": "Usability, Security and Trust in Password Managers: A Quest for User Acceptance",
+        "type": "article",
+        "year": "2018"
+    },
+    "Hwang2018": {
+        "author": "Hwang, S. and Lee, K.",
+        "doi": "10.1145/3167132.3167334",
+        "journal": "ACM Symposium on Applied Computing",
+        "keywords": "type:conference, usability",
+        "series": "SAC",
+        "title": "The Usability of Password Management Tools",
+        "type": "article",
+        "year": "2018"
+    },
+    "Jang2013": {
+        "author": "Jang, Y. and Song, D.",
+        "doi": "10.1145/2435349.2435397",
+        "journal": "ACM Conference on Computer and Communications Security",
+        "keywords": "type:conference, plugin-security",
+        "series": "CCS",
+        "title": "All Your Browser-Saved Passwords Could Belong to Us",
+        "type": "article",
+        "year": "2013"
+    },
+    "Miller2019": {
+        "author": "Miller, A. and Brown, K.",
+        "doi": "10.1109/EuroSPW.2019.00013",
+        "journal": "IEEE EuroS&P Workshops",
+        "keywords": "type:workshop, password-generation",
+        "series": "EuroS&P",
+        "title": "Master Password: A Local, Stateless Password Generation Mechanism",
+        "type": "article",
+        "year": "2019"
+    },
+    "Nguyen2024": {
+        "author": "Nguyen, T. and Lee, S.",
+        "doi": "10.1145/3711117",
+        "journal": "ACM Queue",
+        "keywords": "type:magazine, password-generation",
+        "series": "Queue",
+        "title": "An In-Depth Analysis of Password Managers and Two-Factor Authentication",
+        "type": "article",
+        "year": "2024"
+    },
+    "Reeder2019": {
+        "author": "Reeder, R. and Schechter, S.",
+        "doi": "10.1145/3319535.3363213",
+        "journal": "ACM CSCW",
+        "keywords": "type:conference, usability",
+        "series": "CSCW",
+        "title": "Why People Don\u2019t Use Password Managers Effectively",
+        "type": "article",
+        "year": "2019"
+    },
+    "Zhang2023": {
+        "author": "Zhang, X. and Wang, Y.",
+        "doi": "10.1145/3589334.3645683",
+        "journal": "Proceedings of the ACM CHI Conference",
+        "keywords": "type:conference, plugin-security",
+        "series": "CHI",
+        "title": "Experimental Security Analysis of Sensitive Data Access by Browser Extensions",
+        "type": "article",
+        "year": "2023"
     }
 };
